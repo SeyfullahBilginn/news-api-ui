f2dd89c45ef1de912ad7f9d2b53510c877fe44c6
unknown <seyfullahbilgin06@gmail.com>
2022-10-29 14:57:01 +0300

    refactor: add api key file

---
+++ src/apiKey.js
@@ -0,0 +1,1 @@
+export const NEWS_API_KEY = "12ce774c071c4ce1898833bb2cbc90db"
--- src/services/NewsService.js
+++ src/services/NewsService.js
@@ -1,6 +1,7 @@
-const NEWS_API_KEY = "12ce774c071c4ce1898833bb2cbc90db"
-// const NEWS_API_KEY = "fa45046ece754923ba7bd91166e573f6"
+import { NEWS_API_KEY } from "../apiKey";
+
 const API_BASE_URL = "https://newsapi.org/v2/everything";
+
 
 class NewsService {
   getAllNews( keyword, activePage) {
